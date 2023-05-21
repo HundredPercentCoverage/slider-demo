7cce60aaaac2f85823c658332794896f29dc53a6
HundredPercentCoverage <bunglenutter@gmail.com>
2023-05-21 01:02:19 +0100

    Get middle one working

--- src/components/PointerSlider.tsx
+++ src/components/PointerSlider.tsx
@@ -1,4 +1,4 @@
-import { RefObject, SetStateAction, useRef, useState } from "react"; 
+import { RefObject, SetStateAction, useEffect, useRef, useState } from "react"; 
 import styles from "./PointerSlider.module.css";
 import type { PointerEvent, Dispatch } from "react";
 
@@ -8,44 +8,110 @@ const MAX_VALUE = 200;
 export default function PointerSlider() {
   const thumbLeftRef = useRef<HTMLDivElement>(null);
   const thumbRightRef = useRef<HTMLDivElement>(null);
+  const thumbCentreRef = useRef<HTMLDivElement>(null);
   const trackRef = useRef<HTMLDivElement>(null);
   const [lowerValue, setLowerValue] = useState(MIN_VALUE);
   const [upperValue, setUpperValue] = useState(MAX_VALUE);
 
-  const onPointerDown = (thumbRef: RefObject<HTMLDivElement>, maxValue: number, minValue: number, setter: Dispatch<SetStateAction<number>>) => (e: PointerEvent<HTMLDivElement>) => {
-    thumbRef.current?.setPointerCapture(e.pointerId);
+  const onPointerDownMin = (e: PointerEvent<HTMLDivElement>) => {
+    thumbLeftRef.current?.setPointerCapture(e.pointerId);
 
-    if (thumbRef.current) {
-      thumbRef.current.onpointermove = function(evt) {
-        if (thumbRef.current && trackRef.current) {
-          const rect = trackRef.current.getBoundingClientRect();
-          const trackFraction = (evt.clientX - rect.left) / rect.width;
-          const trackPercentage = trackFraction * 100;
-          const trackValue = trackFraction * MAX_VALUE;
+    if (thumbLeftRef.current) {
+      thumbLeftRef.current.onpointermove = function(evt) {
+        if (thumbLeftRef.current && trackRef.current) {
+          const trackRect = trackRef.current.getBoundingClientRect();
+          const trackValue = (evt.clientX - trackRect.left) / trackRect.width * MAX_VALUE;
 
-          if (trackValue > maxValue) {
-            setter(maxValue);
-          } else if (trackValue < minValue) {
-            setter(minValue)
+          if (trackValue > upperValue) {
+            setLowerValue(upperValue);
+          } else if (trackValue < MIN_VALUE) {
+            setLowerValue(MIN_VALUE);
           } else {
-            thumbRef.current.style.left = trackPercentage + '%';
-            setter(trackValue);
+            setLowerValue(trackValue);
           }
         }
       }
 
-      thumbRef.current.onpointerup = function(evt) {
-        if (thumbRef.current) {
-          thumbRef.current.onpointermove = null;
-          thumbRef.current.onpointerup = null;
+      thumbLeftRef.current.onpointerup = function(evt) {
+        if (thumbLeftRef.current) {
+          thumbLeftRef.current.onpointermove = null;
+          thumbLeftRef.current.onpointerup = null;
         }
         // ...also process the "drag end" if needed
       };
     }
   }
 
-  const onPointerDownMin = onPointerDown(thumbLeftRef, upperValue, MIN_VALUE, setLowerValue);
-  const onPointerDownMax = onPointerDown(thumbRightRef, MAX_VALUE, lowerValue, setUpperValue);
+  const onPointerDownMax = (e: PointerEvent<HTMLDivElement>) => {
+    thumbRightRef.current?.setPointerCapture(e.pointerId);
+
+    if (thumbRightRef.current) {
+      thumbRightRef.current.onpointermove = function(evt) {
+        if (thumbRightRef.current && trackRef.current) {
+          const trackRect = trackRef.current.getBoundingClientRect();
+          const trackValue = (evt.clientX - trackRect.left) / trackRect.width * MAX_VALUE;
+
+          if (trackValue > MAX_VALUE) {
+            setUpperValue(MAX_VALUE);
+          } else if (trackValue < lowerValue) {
+            setUpperValue(lowerValue);
+          } else {
+            setUpperValue(trackValue);
+          }
+        }
+      }
+
+      thumbRightRef.current.onpointerup = function(evt) {
+        if (thumbRightRef.current) {
+          thumbRightRef.current.onpointermove = null;
+          thumbRightRef.current.onpointerup = null;
+        }
+        // ...also process the "drag end" if needed
+      };
+    }
+  }
+
+  const onPointerDownCentre = (e: PointerEvent<HTMLDivElement>) => {
+    thumbCentreRef.current?.setPointerCapture(e.pointerId);
+
+    if (thumbCentreRef.current) {
+      thumbCentreRef.current.onpointermove = function(evt) {
+        if (thumbCentreRef.current && trackRef.current) {
+          const trackRect = trackRef.current.getBoundingClientRect();
+          const trackValue = (evt.clientX - trackRect.left) / trackRect.width * MAX_VALUE;
+
+          const diff = upperValue - lowerValue;
+          const newLowerVal = trackValue - diff/2;
+          const newUpperVal = trackValue + diff/2;
+
+          if (newLowerVal < MIN_VALUE) {
+            // do nothing
+          } else if (newUpperVal > MAX_VALUE) {
+            // do nothing
+          } else {
+            setLowerValue(newLowerVal);
+            setUpperValue(newUpperVal);
+          }
+        }
+      }
+
+      thumbCentreRef.current.onpointerup = function(evt) {
+        if (thumbCentreRef.current) {
+          thumbCentreRef.current.onpointermove = null;
+          thumbCentreRef.current.onpointerup = null;
+        }
+        // ...also process the "drag end" if needed
+      };
+    }
+  }
+
+  useEffect(() => {
+    if (thumbCentreRef.current && thumbLeftRef.current && thumbRightRef.current) {
+      thumbCentreRef.current.style.left = (upperValue - ((upperValue - lowerValue) / 2)) / MAX_VALUE * 100 + '%';
+      thumbLeftRef.current.style.left = lowerValue / MAX_VALUE * 100 + '%';
+      thumbRightRef.current.style.left = upperValue / MAX_VALUE * 100 + '%';
+    }
+  }, [lowerValue, upperValue]);
 
   return (
     <div>
@@ -54,6 +120,11 @@ export default function PointerSlider() {
           className={styles.thumbLeft}
           ref={thumbLeftRef}
           onPointerDown={onPointerDownMin}
+        />
+        <div
+          className={styles.thumbCentre}
+          ref={thumbCentreRef}
+          onPointerDown={onPointerDownCentre}
         />
         <div
           className={styles.thumbRight}
